979e327e90f9482b13bc632243eef00492c45760
Philippe Beck <philippe@philippebeck.net>
2020-06-05 23:07:31 +0200

    Add new methods to ContellationController

--- src/Controller/ConstellationController.php
+++ src/Controller/ConstellationController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Pam\Controller\MainController;
 use Pam\Model\Factory\ModelFactory;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -12,7 +11,7 @@ use Twig\Error\SyntaxError;
  * Class ConstellationController
  * @package App\Controller
  */
-class ConstellationController extends MainController
+class ConstellationController extends BaseController
 {
     /**
      * @return string
@@ -24,8 +23,48 @@ class ConstellationController extends MainController
     {
         $constellations = ModelFactory::getModel('Constellation')->listData();
 
-        return $this->render('constellation.twig', [
-            'constellations' => $constellations
-        ]);
+        return $this->render('constellation/listConstellations.twig', ['constellations' => $constellations]);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function readMethod()
+    {
+        $constellation = ModelFactory::getModel('Constellation')->readData($this->globals->getGet()->getGetVar('id'));
+
+        return $this->render('constellation/constellation.twig', ['constellation' => $constellation]);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function updateMethod()
+    {
+        $this->checkAdminAccess();
+
+        if (!empty($this->globals->getPost()->getPostArray())) {
+
+            if (!empty($this->globals->getFiles()->getFileVar('name'))) {
+                $data['name'] = $this->globals->getFiles()->uploadFile('img/constellation');
+            }
+
+            $data['description'] = $this->globals->getPost()->getPostVar('description');
+
+            ModelFactory::getModel('Constellation')->updateData($this->globals->getGet()->getGetVar('id'), $data);
+            $this->globals->getSession()->createAlert('Successful modification of the selected constellation !', 'blue');
+
+            $this->redirect('admin');
+        }
+
+        $constellation = ModelFactory::getModel('Constellation')->readData($this->globals->getGet()->getGetVar('id'));
+
+        return $this->render('constellation/updateConstellation.twig', ['constellation' => $constellation]);
     }
 }
