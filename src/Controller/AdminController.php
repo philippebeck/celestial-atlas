7a0c855c1575bc5877ef0872ad0a106fd0010d38
Philippe Beck <philippe@philippebeck.net>
2020-06-05 23:01:56 +0200

    Create AdminController

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ */
+class AdminController extends BaseController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        $this->checkAdminAccess();
+
+        $constellations = ModelFactory::getModel('Constellation')->listData();
+        $atlases        = ModelFactory::getModel('Atlas')->listData();
+        $maps           = ModelFactory::getModel('Map')->listData();
+        $users          = ModelFactory::getModel('User')->listData();
+
+        return $this->render('admin/admin.twig', [
+            'constellations'    => $constellations,
+            'atlases'           => $atlases,
+            'maps'              => $maps,
+            'users'             => $users
+        ]);
+    }
+}
